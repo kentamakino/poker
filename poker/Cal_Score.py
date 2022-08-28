2bcb1c1b030406336b56a8175db72d2ecf8a0556
miyamoto <taihei.miyamoto@gmail.com>
2022-08-28 13:48:49 +0900

    update Cal_Score.py

--- poker/Cal_Score.py
+++ poker/Cal_Score.py
@@ -1,3 +1,6 @@
+from Check_Hand import check_hand, comparison_hands
+
+
 # 上段の点数計算
 def score_top(card):
     cards = []
@@ -37,3 +40,12 @@ def score_middle(card_middle_strength):
 def score_bottom(card_bottom_strength):
     score_bottom_table = [0, 0, 0, 0, 2, 4, 6, 10, 15, 25]
     return score_bottom_table[card_bottom_strength]
+
+
+def calc_score(top_card, middle_card, bottom_card):
+    if comparison_hands(card_top, card_middle) and comparison_hands(card_middle, card_bottom):
+        score_total = score_top(check_hand(card_top)[2]) + score_middle(check_hand(card_middle)[1]) + score_bottom(
+            check_hand(card_bottom)[1])
+    else:
+        score_total = 0
+    return score_total
